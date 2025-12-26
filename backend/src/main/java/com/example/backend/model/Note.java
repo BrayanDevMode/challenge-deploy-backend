30a2686100b567e007cc74fb041f5965933a2a8c
BrayanDevMode <bcarlosac20@gmail.com>
2025-12-26 09:01:54 -0500

    Define Note entity

--- backend/src/main/java/com/example/backend/model/Note.java
+++ backend/src/main/java/com/example/backend/model/Note.java
@@ -1,4 +1,17 @@
 package com.example.backend.model;
+import jakarta.persistence.*;
+import lombok.Data;
 
+// Maps this class to a database table
+@Entity
+// Generates getters and setters
+@Data
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String content;
+    private boolean archived = false;
 }
