dd8819e2dd6c54bf8e3f5559772d5ea63043fc08
BrayanDevMode <bcarlosac20@gmail.com>
2025-12-28 13:46:20 -0500

    Changes to deploy

--- frontend/src/services/NoteService.js
+++ frontend/src/services/NoteService.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080/api/notes";
-
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8080/api/notes";
 class NoteService {
     getAll() {
         return axios.get(API_URL);
