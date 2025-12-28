2208f563d9af55694ad39854d331b6c89c885882
BrayanDevMode <bcarlosac20@gmail.com>
2025-12-27 21:54:47 -0500

    Implement NoteService for frontend-backend communication

---
+++ frontend/src/services/NoteService.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const API_URL = "http://localhost:8080/api/notes";
+
+class NoteService {
+    getAll() {
+        return axios.get(API_URL);
+    }
+
+    getArchived() {
+        return axios.get(`${API_URL}/archived`);
+    }
+
+    create(note) {
+        return axios.post(API_URL, note);
+    }
+
+    toggleArchive(id) {
+        return axios.put(`${API_URL}/${id}/archive`);
+    }
+
+    delete(id) {
+        return axios.delete(`${API_URL}/${id}`);
+    }
+}
+
+export default new NoteService();
